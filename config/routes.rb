3ba83d1c22c68d3929805541dcc8ef5bfdf8aa1b
Jacky Alciné <me@jalcine.me>
2013-08-20 16:52:31 -0400

    Updated routes to include AccountsController.
      Allow Recurly to access the POST url necessary to show the success page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Padproof::Application.routes.draw do
   root :to => 'accounts#index'
 
   match '/accounts/sign-up', :to => 'accounts#early_sign_up'
+  post '/accounts/success', :to => 'accounts#success'
 end
