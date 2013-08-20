ede2a02942a74110752b150a6a6b068b53db8f9c
Jacky Alciné <me@jalcine.me>
2013-08-20 17:28:08 -0400

    Added PadproofUAT keys.

--- config/initializers/recurly.rb
+++ config/initializers/recurly.rb
@@ -3,9 +3,9 @@ if Rails.env.production? then
   Recurly.api_key          = ENV['RECURLY_API_KEY']
   Recurly.js.private_key   = ENV['RECURLY_JS_PRIVATE_KEY']
 else
-  Recurly.subdomain      = "subdomain"
-  Recurly.api_key        = "api-key"
-  Recurly.js.private_key = "private-key"
+  Recurly.subdomain      = "padproofuat"
+  Recurly.api_key        = "4dca8d4b28cd401091c43835cf4e6fe5"
+  Recurly.js.private_key = "222ce621e54c4e5aaa8366bb3a32e81d"
 end
 
 # Recurly.default_currency = 'USD'
